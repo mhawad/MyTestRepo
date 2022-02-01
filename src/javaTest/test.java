e8ec63e16e1a6a7fa300b73e8df65db8be09f2b1
mhawad <mirnahelmybio@yahoo.com>
2022-02-01 14:44:15 -0500

    Add files via upload

---
+++ src/javaTest/test.java
@@ -0,0 +1,14 @@
+package javaTest;
+
+public class test {
+
+	public static void main(String[] arg) {
+
+		int i = 0;
+		
+		for (i=5 ; i<10 ; i++)
+
+		System.out.println("Number is: " + i);
+	}
+
+}
